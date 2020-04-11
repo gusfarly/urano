38b3dd8aaa0cc89e963a2cc92eed4cd75ded6bc4
Unknown <gmorenooliva@gmail.com>
2020-04-11 20:31:33 +0200

    se quita eureka y se actualiza spring boot admin

--- src/main/java/com/ggastudios/urano/UranoApplication.java
+++ src/main/java/com/ggastudios/urano/UranoApplication.java
@@ -2,14 +2,17 @@ package com.ggastudios.urano;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.*;
 
 
-// todo quitar eureka, no es necesario para esta aplicacion de momento al no utilizar microservicios(no son viables por el plan de heroku, ya que cada vez que se cae un micro deja de estar registrado.)
 @SpringBootApplication
 //@ComponentScan(basePackages = {"com.ggastudios.urano.controller","com.ggastudios.urano.service","com.ggastudios.urano.controller.advice"})
 @ComponentScan(basePackages = {"com.ggastudios.urano.*"})
 @Import({MvcConfigurer.class})
+//@EnableEurekaClient
+@EnableDiscoveryClient
 public class UranoApplication {
 
 	public static void main(String[] args) {
